0ee421b085d4e2181475398c23cb4a2755261417
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-05-21 13:43:03 +0200

    Update module path names in config.php
    The module and listener path names in config.php have been updated to use '.' as the delimiter instead of ''. This makes the names more consistent and easy to read, and aligns with standard naming conventions.

--- Contao/config.php
+++ Contao/config.php
@@ -8,17 +8,17 @@ ArrayUtil::arrayInsert(
     [
         'wem_fdm_form_data_manager' => [
             'tables' => ['tl_sm_form_storage', 'tl_sm_form_storage_data'],
-            'export_all' => ['wemformdatamanager.backend.module.form_data_manager.backend_controller', 'exportAll'],
-            'export' => ['wemformdatamanager.backend.module.form_data_manager.backend_controller', 'exportSingle'],
+            'export_all' => ['wem.form_data_manager.backend.module.form_data_manager.backend_controller', 'exportAll'],
+            'export' => ['wem.form_data_manager.backend.module.form_data_manager.backend_controller', 'exportSingle'],
         ],
     ]
 );
-$GLOBALS['BE_MOD']['content']['form']['export_all'] = ['wemformdatamanager.backend.module.form_data_manager.backend_controller', 'exportAllFromForm'];
-$GLOBALS['BE_MOD']['content']['form']['export'] = ['wemformdatamanager.backend.module.form_data_manager.backend_controller', 'exportSingle'];
+$GLOBALS['BE_MOD']['content']['form']['export_all'] = ['wem.form_data_manager.backend.module.form_data_manager.backend_controller', 'exportAllFromForm'];
+$GLOBALS['BE_MOD']['content']['form']['export'] = ['wem.form_data_manager.backend.module.form_data_manager.backend_controller', 'exportSingle'];
 
-$GLOBALS['TL_HOOKS']['processFormData'][] = ['wemformdatamanager.listener.process_form_data', '__invoke'];
-$GLOBALS['TL_HOOKS']['compileFormFields'][] = ['wemformdatamanager.listener.compile_form_fields', '__invoke'];
-$GLOBALS['TL_HOOKS']['sendNotificationMessage'][] = ['wemformdatamanager.listener.send_notification_message', '__invoke'];
+$GLOBALS['TL_HOOKS']['processFormData'][] = ['wem.form_data_manager.listener.process_form_data', '__invoke'];
+$GLOBALS['TL_HOOKS']['compileFormFields'][] = ['wem.form_data_manager.listener.compile_form_fields', '__invoke'];
+$GLOBALS['TL_HOOKS']['sendNotificationMessage'][] = ['wem.form_data_manager.listener.send_notification_message', '__invoke'];
 
 $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['contao']['core_form']['email_text'][] = 'useful_data'; // kept for BC compatibility
 $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['contao']['core_form']['email_text'][] = 'useful_data_text';
@@ -31,19 +31,19 @@ $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['contao']['core_form']['fil
 
 
 // PDM EXPORT
-$GLOBALS['WEM_HOOKS']['formatSinglePersonalDataForCsvExport'][] = ['wemformdatamanager.listener.personal_data_csv_formatter', 'formatSingle'];
-$GLOBALS['WEM_HOOKS']['exportByPidAndPtableAndEmail'][] = ['wemformdatamanager.listener.personal_data_export', 'exportByPidAndPtableAndEmail'];
+$GLOBALS['WEM_HOOKS']['formatSinglePersonalDataForCsvExport'][] = ['wem.form_data_manager.listener.personal_data_csv_formatter', 'formatSingle'];
+$GLOBALS['WEM_HOOKS']['exportByPidAndPtableAndEmail'][] = ['wem.form_data_manager.listener.personal_data_export', 'exportByPidAndPtableAndEmail'];
 // PDM ANONYMIZE
-$GLOBALS['WEM_HOOKS']['anonymizeByPidAndPtableAndEmail'][] = ['wemformdatamanager.listener.personal_data_anonymize', 'anonymizeByPidAndPtableAndEmail'];
+$GLOBALS['WEM_HOOKS']['anonymizeByPidAndPtableAndEmail'][] = ['wem.form_data_manager.listener.personal_data_anonymize', 'anonymizeByPidAndPtableAndEmail'];
 // PDM UI
-$GLOBALS['WEM_HOOKS']['sortData'][] = ['wemformdatamanager.listener.personal_data_ui', 'sortData'];
-$GLOBALS['WEM_HOOKS']['renderSingleItemTitle'][] = ['wemformdatamanager.listener.personal_data_ui', 'renderSingleItemTitle'];
-$GLOBALS['WEM_HOOKS']['renderSingleItemBodyOriginalModelSingle'][] = ['wemformdatamanager.listener.personal_data_ui', 'renderSingleItemBodyOriginalModelSingle'];
-$GLOBALS['WEM_HOOKS']['renderSingleItemBodyOriginalModelSingleFieldValue'][] = ['wemformdatamanager.listener.personal_data_ui', 'renderSingleItemBodyOriginalModelSingleFieldValue'];
-$GLOBALS['WEM_HOOKS']['renderSingleItemBodyPersonalDataSingle'][] = ['wemformdatamanager.listener.personal_data_ui', 'renderSingleItemBodyPersonalDataSingle'];
-$GLOBALS['WEM_HOOKS']['buildSingleItemBodyPersonalDataSingleButtons'][] = ['wemformdatamanager.listener.personal_data_ui', 'buildSingleItemBodyPersonalDataSingleButtons'];
-$GLOBALS['WEM_HOOKS']['renderSingleItemBodyPersonalDataSingleFieldLabel'][] = ['wemformdatamanager.listener.personal_data_ui', 'renderSingleItemBodyPersonalDataSingleFieldLabel'];
-$GLOBALS['WEM_HOOKS']['renderSingleItemBodyPersonalDataSingleFieldValue'][] = ['wemformdatamanager.listener.personal_data_ui', 'renderSingleItemBodyPersonalDataSingleFieldValue'];
+$GLOBALS['WEM_HOOKS']['sortData'][] = ['wem.form_data_manager.listener.personal_data_ui', 'sortData'];
+$GLOBALS['WEM_HOOKS']['renderSingleItemTitle'][] = ['wem.form_data_manager.listener.personal_data_ui', 'renderSingleItemTitle'];
+$GLOBALS['WEM_HOOKS']['renderSingleItemBodyOriginalModelSingle'][] = ['wem.form_data_manager.listener.personal_data_ui', 'renderSingleItemBodyOriginalModelSingle'];
+$GLOBALS['WEM_HOOKS']['renderSingleItemBodyOriginalModelSingleFieldValue'][] = ['wem.form_data_manager.listener.personal_data_ui', 'renderSingleItemBodyOriginalModelSingleFieldValue'];
+$GLOBALS['WEM_HOOKS']['renderSingleItemBodyPersonalDataSingle'][] = ['wem.form_data_manager.listener.personal_data_ui', 'renderSingleItemBodyPersonalDataSingle'];
+$GLOBALS['WEM_HOOKS']['buildSingleItemBodyPersonalDataSingleButtons'][] = ['wem.form_data_manager.listener.personal_data_ui', 'buildSingleItemBodyPersonalDataSingleButtons'];
+$GLOBALS['WEM_HOOKS']['renderSingleItemBodyPersonalDataSingleFieldLabel'][] = ['wem.form_data_manager.listener.personal_data_ui', 'renderSingleItemBodyPersonalDataSingleFieldLabel'];
+$GLOBALS['WEM_HOOKS']['renderSingleItemBodyPersonalDataSingleFieldValue'][] = ['wem.form_data_manager.listener.personal_data_ui', 'renderSingleItemBodyPersonalDataSingleFieldValue'];
 // PDM Manager
-$GLOBALS['WEM_HOOKS']['getFileByPidAndPtableAndEmailAndField'][] = ['wemformdatamanager.listener.personal_data_manager', 'getFileByPidAndPtableAndEmailAndField'];
-$GLOBALS['WEM_HOOKS']['isPersonalDataLinkedToFile'][] = ['wemformdatamanager.listener.personal_data_manager', 'isPersonalDataLinkedToFile'];
+$GLOBALS['WEM_HOOKS']['getFileByPidAndPtableAndEmailAndField'][] = ['wem.form_data_manager.listener.personal_data_manager', 'getFileByPidAndPtableAndEmailAndField'];
+$GLOBALS['WEM_HOOKS']['isPersonalDataLinkedToFile'][] = ['wem.form_data_manager.listener.personal_data_manager', 'isPersonalDataLinkedToFile'];
