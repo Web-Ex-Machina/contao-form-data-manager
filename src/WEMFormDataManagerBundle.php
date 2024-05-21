5e3715cef2ba15ebc34a93b691595eab0aef9a6e
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-05-21 13:43:55 +0200

    Add container extension to FormDataManagerBundle
    With Symfony's dependency injection, the defined service in 'WEMFormDataManagerBundle.php' is now able to return a new 'WEMFormDataManagerExtension' instance. This change is necessary to inject services and configurations properly in the bundle.

--- src/WEMFormDataManagerBundle.php
+++ src/WEMFormDataManagerBundle.php
@@ -3,7 +3,9 @@
 
 namespace Wem\ContaoFormDataManagerBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Wem\ContaoFormDataManagerBundle\DependencyInjection\WEMFormDataManagerExtension;
 
 //use old bundle and not AbstractBundle because contao 4.13 is on symfony 5
 class WEMFormDataManagerBundle extends Bundle
@@ -11,5 +13,10 @@ class WEMFormDataManagerBundle extends Bundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new WEMFormDataManagerExtension();
     }
 }
