166afe70788880bff292896a4188c37c2a36ab79
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-05-27 15:11:57 +0200

    refactor: Change namespace in BackendController.php

--- src/Backend/BackendController.php
+++ src/Backend/BackendController.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * @link     https://github.com/Web-Ex-Machina/wem-contao-form-data-manager/
  */
 
-namespace WEM\ContaoFormDataManagerBundle\FormDataManager;
+namespace WEM\ContaoFormDataManagerBundle\Backend;
 
 use Contao\Config;
 use Contao\CoreBundle\Controller\BackendController as ControllerBackendController;
