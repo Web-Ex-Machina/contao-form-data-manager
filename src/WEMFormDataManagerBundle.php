eab70daeb97cad7dd6577ae1f29b59ed71cfe752
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-05-27 12:06:11 +0200

    feat: Update composer details and organize resource files

--- src/WEMFormDataManagerBundle.php
+++ src/WEMFormDataManagerBundle.php
@@ -3,21 +3,8 @@
 
 namespace WEM\ContaoFormDataManagerBundle;
 
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use WEM\ContaoFormDataManagerBundle\DependencyInjection\WEMFormDataManagerExtension;
 
-//use old bundle and not AbstractBundle because contao 4.13 is on symfony 5
 class WEMFormDataManagerBundle extends Bundle
 {
-    /**
-     * Retrieve the container extension for this Symfony Bundle.
-     * Needed because the non standard naming
-     *
-     * @return WEMFormDataManagerExtension|null The container extension object or null if not available.
-     */
-    public function getContainerExtension(): ?ExtensionInterface
-    {
-        return new WEMFormDataManagerExtension();
-    }
 }
