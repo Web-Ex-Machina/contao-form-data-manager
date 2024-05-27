746473c8646b52ebd1fa143dc5a67c6954c4ecc7
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-05-27 12:10:38 +0200

    refactor: change FileLocator path in WEMFormDataManagerExtension

--- src/DependencyInjection/WEMFormDataManagerExtension.php
+++ src/DependencyInjection/WEMFormDataManagerExtension.php
@@ -11,7 +11,10 @@ class WEMFormDataManagerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
         $loader->load('services.yaml');
     }
 }
