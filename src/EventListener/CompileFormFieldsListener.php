157fa3fc6deb4129ea319d40cd9af7b99cae5274
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-05-28 09:17:44 +0200

    Refactor CompileFormFieldsListener and update services.yaml

--- src/EventListener/CompileFormFieldsListener.php
+++ src/EventListener/CompileFormFieldsListener.php
@@ -15,20 +15,13 @@ declare(strict_types=1);
 namespace WEM\ContaoFormDataManagerBundle\EventListener;
 
 use Contao\Form;
-
+use Contao\System;
+use Contao\Environment;
+use Contao\FormFieldModel;
 
 class CompileFormFieldsListener
 {
-    protected array $listeners;
-
-    public function __construct(
-
-        array $listeners
-    ) {
-
-
-        $this->listeners = $listeners;
-    }
+    public function __construct() {}
 
     /**
      * Sets the fdm_formdatamanager javascript file for the given form.
@@ -37,14 +30,56 @@ class CompileFormFieldsListener
      * @param string $formId The ID of the form.
      * @param Form $form The form object.
      *
-     * @return void
+     * @return array
      */
     public function __invoke(
         array $arrFields,
         string $formId,
         Form $form
-    ): void {
+    ): array {
         $GLOBALS['TL_JAVASCRIPT']['fdm_formdatamanager'] = 'bundles/ContaoFormDataManager/js/module/formdatamanager/frontend.js';
+
+        if ((bool) $form->getModel()->storeViaFormDataManager) {
+
+            global $objPage;
+
+            $objFormFieldFirstAppearance = (new FormFieldModel());
+            $objFormFieldFirstAppearance->name = 'fdm[first_appearance]';
+            $objFormFieldFirstAppearance->type = 'hidden';
+            $objFormFieldFirstAppearance->is_technical_field = 1;
+            $arrFields['first_appearance'] = $objFormFieldFirstAppearance;
+
+            $objFormFieldFirstInteraction = (new FormFieldModel());
+            $objFormFieldFirstInteraction->name = 'fdm[first_interaction]';
+            $objFormFieldFirstInteraction->type = 'hidden';
+            $objFormFieldFirstInteraction->is_technical_field = 1;
+            $arrFields['first_interaction'] = $objFormFieldFirstInteraction;
+
+            $objFormFieldCurrentPage = (new FormFieldModel());
+            $objFormFieldCurrentPage->name = 'fdm[current_page]';
+            $objFormFieldCurrentPage->type = 'hidden';
+            $objFormFieldCurrentPage->is_technical_field = 1;
+            $objFormFieldCurrentPage->value = $objPage ? $objPage->id : 0;
+            $arrFields['current_page'] = $objFormFieldCurrentPage;
+
+            $objFormFieldCurrentPage = (new FormFieldModel());
+            $objFormFieldCurrentPage->name = 'fdm[current_page_url]';
+            $objFormFieldCurrentPage->type = 'hidden';
+            $objFormFieldCurrentPage->is_technical_field = 1;
+            $objFormFieldCurrentPage->value = Environment::get('uri');
+            $arrFields['current_page_url'] = $objFormFieldCurrentPage;
+
+            // Previous page
+            $objFormFieldRefererPage = (new FormFieldModel());
+            $objFormFieldRefererPage->name = 'fdm[referer_page_url]';
+            $objFormFieldRefererPage->type = 'hidden';
+            $objFormFieldRefererPage->is_technical_field = 1;
+            $objFormFieldRefererPage->value = System::getReferer();
+            $arrFields['referer_page_url'] = $objFormFieldRefererPage;
+        }
+
+        return $arrFields;
+
     }
 
 }
